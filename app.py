2734e99be379c028ad9bae51c22537dfdd74a501
NickAzz50 <32042271+NickAzz50@users.noreply.github.com>
2023-08-07 20:02:10 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,14 @@
-from flask import Flask, jsonify, render_template, request
-from datetime import datetime
-import pandas
-app = Flask(__name__)
-"""
-PAGES
-Home => Mission Statement
-Survey => Allow users to take the survey
-Results => Displays the results shown
-"""
+from flask import Flask, render_template
 
+app = Flask(__name__)
+
+# Define your routes
+@app.route('/')
+def index():
+    return render_template('mission.html')
 
 @app.route('/home')
 def home():
-    datetime.now()
     return render_template('mission.html')
 
 @app.route('/survey')
@@ -22,10 +18,6 @@ def survey():
 @app.route('/results')
 def results():
     return render_template('results.html')
-
-
-
-
 
 if __name__ == "__main__":
     app.run()
